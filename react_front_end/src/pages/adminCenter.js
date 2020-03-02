b8f3588bcdee4c8261443888e488851e4f6f66bd
Weiying <654033305@qq.com>
2020-03-02 22:50:35 +1100

    add creating and editting blogpost page

--- react_front_end/src/pages/adminCenter.js
+++ react_front_end/src/pages/adminCenter.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Row, Col, Modal, Button } from "antd";
+import {Link} from 'react-router-dom'
 import UploadButton from "../components/uploadButton";
 import AdminPostsList from "../components/adminPostsList";
 
@@ -17,6 +18,9 @@ function AdminCenter() {
         <UploadButton/>
       </Row>
  
+      <Row type="flex" justify="center" >
+        <Link to="/adminCenter/editPosts/new"><Button type="primary">Create a New Blog Post</Button> </Link>
+      </Row>
           
       
     </div>
--- react_front_end/src/pages/editBlogPostPage.js
+++ react_front_end/src/pages/editBlogPostPage.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { Fragment,useState,useEffect } from 'react'
 import {useParams} from 'react-router-dom'
+import ReactMarkdown from 'react-markdown'
 
+
+import {Row,Button,Input} from 'antd' 
 export const EditBlogPostPage = (props) => {
+    const [text,setText] = useState('Text Area')
     const post_id = useParams().id
+
+    const [newFileFlag,setNewFileFlag] = useState(false)
+
+    const handleChange = (e) =>{
+     
+        setText(e.target.value)
+    }
+
+    useEffect(() => {
+        if(post_id === "new"){
+            setNewFileFlag(true)
+        }
+
+    }, [])
     return(
-        <div>
-            {post_id}
-        </div>
+        <Fragment>
+            
+            <ReactMarkdown className="markdown_div"  style={{overflow:"hidden"}} source={text}></ReactMarkdown>
+
+        
+        <Row style={{marginTop:"10%"}}>
+            <Input.TextArea onChange={ e => handleChange(e)}></Input.TextArea>
+        </Row>
+        
+        
+        
+        <Row style={{marginTop:"10px"}}type="flex" justify="space-around">
+            {newFileFlag ? <Button type="primary">
+                Create
+            </Button>: <Button type="primary">
+                Edit
+            </Button>}
+            
+
+
+            <Button type="danger">
+                Cancel
+            </Button>
+        </Row>
+        </Fragment>
     )
 }
 
